64247fe030bbae85ad54ffab66fc0978e83daae1
xroah <396069583@qq.com>
2026-09-04 17:30:20 +0800

    update

--- src-tauri/src/zip/archive.rs
+++ src-tauri/src/zip/archive.rs
@@ -1,8 +1,10 @@
 use std::{
-    collections::BTreeMap,
+    cell::RefCell,
+    collections::HashMap,
     error::Error,
     fs::File,
     path::{Component, Path, PathBuf},
+    rc::{Rc, Weak},
 };
 
 use serde::{Deserialize, Serialize};
@@ -26,21 +28,24 @@ impl PadLeadingZero for u8 {
 pub struct ArchiveNode {
     pub name: String,
     pub is_dir: bool,
-    pub last_modified: Option<String>,
-    pub size: u64,
-    pub compressed_size: u64,
-    pub children: BTreeMap<String, ArchiveNode>,
+    pub last_modified: String,
+    pub size: RefCell<u64>,
+    pub compressed_size: RefCell<u64>,
+    pub children: RefCell<HashMap<String, Rc<ArchiveNode>>>,
+    #[serde(skip_serializing)]
+    pub parent: RefCell<Weak<ArchiveNode>>,
 }
 
-pub fn get_zip_structure(file_path: PathBuf) -> Result<ArchiveNode, Box<dyn Error>> {
-    let mut root = ArchiveNode {
+pub fn get_zip_structure(file_path: PathBuf) -> Result<Rc<ArchiveNode>, Box<dyn Error>> {
+    let root = Rc::new(ArchiveNode {
         name: String::from("root"),
         is_dir: true,
-        last_modified: None,
-        size: 0,
-        compressed_size: 0,
-        children: BTreeMap::new(),
-    };
+        last_modified: String::new(),
+        size: RefCell::new(0),
+        compressed_size: RefCell::new(0),
+        children: RefCell::new(HashMap::new()),
+        parent: RefCell::new(Weak::new()),
+    });
     let file = File::open(file_path)?;
     let mut zip_file: ZipArchive<File> = ZipArchive::new(file)?;
     let len = zip_file.len();
@@ -57,43 +62,49 @@ pub fn get_zip_structure(file_path: PathBuf) -> Result<ArchiveNode, Box<dyn Erro
                 _ => None,
             })
             .collect();
-        let mut current_node = &mut root;
-        let len = components.len();
+        let mut current_node = root.clone();
         let is_dir = f.is_dir();
         let size = f.size();
         let compressed_size = f.compressed_size();
         let last_modified = f.last_modified();
 
-        for (idx, c) in components.iter().enumerate() {
-            let is_last = idx == len - 1;
-            let is_file = is_last && !is_dir;
-            current_node = current_node
+        for c in components {
+            let rc_node = Rc::new(ArchiveNode {
+                name: c.to_string(),
+                is_dir: is_dir,
+                last_modified: format_date_time(last_modified),
+                size: RefCell::new(if !is_dir { size } else { 0 }),
+                compressed_size: RefCell::new(if !is_dir { compressed_size } else { 0 }),
+                children: RefCell::new(HashMap::new()),
+                parent: RefCell::new(Weak::new()),
+            });
+            let parent = Rc::downgrade(&current_node);
+            *rc_node.parent.borrow_mut() = parent;
+
+            if let Some(p) = rc_node.parent.borrow().upgrade() {
+                *p.size.borrow_mut() += size;
+                *p.compressed_size.borrow_mut() += compressed_size;
+            }
+
+            let children = current_node
                 .children
+                .borrow_mut()
                 .entry(c.to_string())
-                .or_insert(ArchiveNode {
-                    name: c.to_string(),
-                    is_dir: !is_last || is_dir,
-                    last_modified: if is_file {
-                        format_date_time(last_modified)
-                    } else {
-                        None
-                    },
-                    size: if is_file { size } else { 0 },
-                    compressed_size: if is_file { compressed_size } else { 0 },
-                    children: BTreeMap::new(),
-                });
+                .or_insert(rc_node)
+                .clone();
+            current_node = children;
         }
     }
 
     Ok(root)
 }
 
-fn format_date_time(dt: Option<DateTime>) -> Option<String> {
+fn format_date_time(dt: Option<DateTime>) -> String {
     let Some(dt) = dt else {
-        return None;
+        return String::new();
     };
 
-    Some(format!(
+    format!(
         "{}-{}-{} {}:{}:{}",
         dt.year(),
         dt.month().pad_leading_zero(),
@@ -101,7 +112,7 @@ fn format_date_time(dt: Option<DateTime>) -> Option<String> {
         dt.hour().pad_leading_zero(),
         dt.minute().pad_leading_zero(),
         dt.second().pad_leading_zero()
-    ))
+    )
 }
 
 pub fn get_zip_json_structure(zip_file: PathBuf) -> Result<String, Box<dyn Error>> {
